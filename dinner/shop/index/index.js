893a7d89f00395eedd0642ac8c782a1d84499ab3
Zranran <40849827+Zranran@users.noreply.github.com>
2018-07-05 17:35:40 +0800

    基本流程（颜色更改）

--- dinner/shop/index/index.js
+++ dinner/shop/index/index.js
@@ -1,11 +1,4 @@
-/**
- *
- * 配套视频教程请移步微信->小程序->灵动云课堂
- * 关注订阅号【huangxiujie85】，第一时间收到教程推送
- *
- * @link http://blog.it577.net
- * @author 黄秀杰
- */
+
 
 var Bmob = require('../../utils/bmob.js');
 var that;
